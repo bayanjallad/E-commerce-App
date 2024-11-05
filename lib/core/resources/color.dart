cc79476d2208f27c27eb0cf5ccc3e2593c9bbd6a
bayanjd <bayanjallad535@gmail.com>
2024-11-04 22:12:13 -0800

    ini file

---
+++ lib/core/resources/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF9775FA);
+const Color red = Color(0xFFEA4335);
+const Color green = Color(0xFF34C559);
+const Color black = Color(0xFF1D1E20);
+const Color gray = Color(0xFF8F959E);
+const Color yelow = Color(0xFFFF981F);
+const Color orange = Color(0xFFFF7043);
